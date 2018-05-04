30621cdc7a194607711c011ea8e83aaf45463e74
hungpq96 <quanghwng22@gmail.com>
2018-05-04 23:54:31 +0700

    Change admin model to administrator for matching previous database

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -59,7 +59,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the application controller.
-  config.authentication_method = :authenticate_admin_user!
+  config.authentication_method = :authenticate_administrator!
 
   # == User Authorization
   #
@@ -91,7 +91,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # (within the application controller) to return the currently logged in user.
-  config.current_user_method = :current_admin_user
+  config.current_user_method = :current_administrator
 
   # == Logging Out
   #
@@ -103,7 +103,7 @@ ActiveAdmin.setup do |config|
   # will call the method to return the path.
   #
   # Default:
-  config.logout_link_path = :destroy_admin_user_session_path
+  config.logout_link_path = :destroy_administrator_session_path
 
   # This setting changes the http method used when rendering the
   # link. For example :get, :delete, :put, etc..
@@ -117,7 +117,7 @@ ActiveAdmin.setup do |config|
   # roots for each namespace.
   #
   # Default:
-  config.root_to = 'admin_users#index'
+  config.root_to = 'administrators#index'
 
   # == Admin Comments
   #
